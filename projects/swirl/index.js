43a25137869da3b7638683e72db9fb4a2b9e9c9a
gladcow <jahr@yandex.ru>
2025-05-27 21:47:55 +0300

    add swirl

---
+++ projects/swirl/index.js
@@ -0,0 +1,16 @@
+const iota = require('../helper/chain/iotaevm')
+
+async function tvl() {
+    const pool = await iota.getObject('0x8c25ec843c12fbfddc7e25d66869f8639e20021758cac1a3db0f6de3c9fda2ed');
+    const iotaAmount = pool.fields.total_supply.fields.value / 10 ** 9;
+    return {
+        iota: iotaAmount,
+    }
+}
+
+module.exports = {
+    methodology: "Calculates the amount of IOTA staked in stIota liquid staking contracts.",
+    iotaevm: {
+        tvl,
+    }
+}
